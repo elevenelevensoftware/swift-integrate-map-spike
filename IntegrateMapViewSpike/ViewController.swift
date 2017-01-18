39560551c1e1875e35e71064ac6b54bc992a7624
Ian Wells <ianwells@ians-mbp>
2017-01-18 20:06:26 +0000

    Initial Commit

---
+++ IntegrateMapViewSpike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IntegrateMapViewSpike
+//
+//  Created by Ian Wells on 18/01/2017.
+//  Copyright © 2017 eleven:eleven software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ IntegrateMapViewSpikeTests/IntegrateMapViewSpikeTests.swift
@@ -0,0 +1,40 @@
+//
+//  IntegrateMapViewSpikeTests.swift
+//  IntegrateMapViewSpikeTests
+//
+//  Created by Ian Wells on 18/01/2017.
+//  Copyright © 2017 eleven:eleven software. All rights reserved.
+//
+
+import XCTest
+@testable import IntegrateMapViewSpike
+
+class IntegrateMapViewSpikeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testWhenAppLoadsThenCoordinatesShouldBeLoadedAlso() {
+        
+    }
+    
+}
